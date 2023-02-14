34111227508146469fa9ce07635f228eb753c548
Vishnu <vishnubishnoi7@gmail.com>
2023-02-14 11:36:39 +0530

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Vishnu Dutt on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
